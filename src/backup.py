ffca79d76cde32968542a6fd04366b9d0327f2b6
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2021-09-05 21:52:41 -0300

    Update backup files

--- src/backup.py
+++ src/backup.py
@@ -12,37 +12,53 @@ from subprocess import run
 
 
 
+user = 'marcelotsvaz'
+
 folders = {
-	'home/marcelotsvaz/.config': [
+	f'home/{user}/.config': [
 		# Fish.
 		'fish/functions/fish_prompt.fish',
-		'fish/functions/exa.fish',
+		'fish/functions/l.fish',
+		'fish/functions/ll.fish',
+		'fish/functions/la.fish',
 		'fish/fish_variables',
 		
 		# KDE.
-		# 'menus/*',
-		# 'plasmashellrc',
-		# 'plasma-org.kde.plasma.desktop-appletsrc',
-		# 'kdeglobals',	# Fonts.
-		# 'plasmarc',
-		# 'kwinrc',
-		# 'kiorc',
-		# 'kscreenlockerrc',
-		# 'kglobalshortcutsrc',
+		'plasmashellrc',							# Screen connectors. Applets size.
+		'systemsettingsrc',							# Desktop layouts.
+		'plasma-org.kde.plasma.desktop-appletsrc',	# Desktop applets.
+		'kdeglobals',								# Theme colors. Fonts. File dialog.
+		'plasmarc',									# Plasma style. Wallpapers.
+		'kwinrc',									# Compositor.
+		'kcminputrc',								# Mouse and keyboard settings.
+		'ksplashrc',								# Splash screen.
+		'kscreenlockerrc',							# Screen locker.
+		'kdedefaults/kdeglobals',					# Themes.
+		'kdedefaults/plasmarc',						# Plasma style?
+		'kdedefaults/kwinrc',						# Window decoration theme?
+		'kdedefaults/kcminputrc',					# Cursor theme.
+		'kdedefaults/kscreenlockerrc',				# Screenlocker theme.
+		'kxkbrc',									# Keyboard layout.
+		'kiorc',									# Trash confirmations.
+		'menus/applications-kmenuedit.menu',		# Applications menu.
+		'kactivitymanagerd-statsrc',				# Launcher favorites.
 		# 'kmixrc',
-		# 'konsolerc',
+		# 'kglobalshortcutsrc',
 		# 'klaunchrc',
-		# 'kcminputrc',
-		# 'kxkbrc',
 		# 'ksmserverrc',
-		# 'ksplashrc',
 		# 'kwinrulesrc',
-		# 'krunnerrc',
 		# 'kservicemenurc',
 		
-		# Other.
-		# 'gtk-4.0/settings.ini',
-		# 'xsettingsd',
+		# Applications.
+		'dolphinrc',
+		'konsolerc',
+		# 'krunnerrc',
+		
+		# Themes.
+		'gtk-3.0/',						# Theme colors.
+		'gtk-4.0/settings.ini',			# Theme colors.
+		'xsettingsd/xsettingsd.conf',	# Theme colors.
+		'Trolltech.conf',				# Theme colors.
 		
 		# VSCode.
 		'Code - OSS/User/settings.json',
@@ -51,15 +67,15 @@ folders = {
 		'Wallpaper.png',
 	],
 	
-	# 'home/marcelotsvaz/.local': [
-	# 	# KDE.
-	# 	'share/plasma-systemmonitor/overview.page',
-	# ],
+	f'home/{user}/.local/share': [
+		# KDE.
+		'plasma-systemmonitor/overview.page',
+	],
 	
-	# '': [
-	# 	# SDDM.
-	# 	'etc/sddm.conf.d/kde_settings.conf',
-	# ],
+	'': [
+		# SDDM.
+		'etc/sddm.conf.d/kde_settings.conf',
+	],
 }
 
 fullFiles = []
@@ -83,20 +99,10 @@ run( args )
 
 
 
-
-
-
-
 # Not on Reddit.
 # plasmanotifyrc
 # kded5rc
-# gtk-3.0/*
-# gtk-4.0/*
-# kdedefaults/*
-# xsettingsd/xsettingsd.conf
 # kmixctrlrc
-# dolphinrc
-
 
 # From Reddit.
 # breezerc
@@ -115,4 +121,3 @@ run( args )
 # startupconfig
 # startupconfigfiles
 # startupconfigkeys
-# systemsettingsrc
