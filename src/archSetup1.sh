fd72c139f48c298c54007362d53675242a087051
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2023-03-09 19:26:05 -0300

    Install systemd-ukify

--- src/archSetup1.sh
+++ src/archSetup1.sh
@@ -60,7 +60,7 @@ ln -s /dev/null /etc/pacman.d/hooks/90-mkinitcpio-install.hook
 
 
 # Install packages.
-system='linux linux-firmware intel-ucode base-devel linux-headers mkinitcpio efibootmgr'
+system='linux linux-firmware intel-ucode base-devel linux-headers mkinitcpio efibootmgr systemd-ukify'
 shell='fish exa htop tmux picocom'
 tools='sudo nano rsync gdisk man-db man-pages'
 developmentTools='git mercurial aws-cli openssh python-pip python-pylint python-isort yapf'
